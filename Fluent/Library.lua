f8344394217481c220a700828e0711d4070882b2
AccountCelestial <v2mjpqjfry@privaterelay.appleid.com>
2025-12-07 08:43:09 +0900

    Update Library.lua

--- Fluent/Library.lua
+++ Fluent/Library.lua
@@ -4247,8 +4247,11 @@ local aa = {
     [47] = function()
         local aa, ab, ac, ad, ae = b(47)
         local af = {
-            Names = {'Dark', 'Darker', 'Light'}
+            Names = {'Default', 'Dark', 'Light'}
         }
+        if af.Current == 'Dark' then
+            af.Current = 'Darker'
+        end
         for ag, ah in next, ab:GetChildren() do
             local aj = ac(ah)
             af[aj.Name] = aj
